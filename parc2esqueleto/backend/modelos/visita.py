8b3e80cc8113503da48dd88cbea59ece0b664535
Miguel <msh21103uma@uma.es>
2025-12-10 16:14:24 +0100

    fuera

--- parc2esqueleto/backend/modelos/visita.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class Visita(BaseModel):
-    correo_visitado: str
-    correo_visitante: str
-    fecha_hora: datetime
-    token_oauth: str
